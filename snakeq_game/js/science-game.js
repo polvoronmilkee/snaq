b4ae8a23ea79eb6f5a25765b386886f6ee4c3d25
Sophia Mendoza <sophiamarielle.mendoza-24@cpu.edu.ph>
2025-08-30 21:20:07 +0800

    Revert "Merge branch 'main' of https://github.com/polvoronmilkee/snakeq"
    This reverts commit e1110a6d670aefc132eaa14331c104c1082fd5d8, reversing
    changes made to 43563615aedcbf43adebacd587b2be74be4f5911.

--- snakeq_game/js/science-game.js
+++ snakeq_game/js/science-game.js
@@ -510,16 +510,10 @@ generateQuestion() {
     q => !this.usedWords[difficulty][questionType].includes(q.question)
   );
 
-  
-    if (unusedWords.length === 0) {
-        console.log("All questions have been used! You're one Brainy SnaQ! Resetting...");
-
-        // Reset so we can reuse all questions
-        this.usedWords[difficulty][questionType] = [];
-
-        // After reset, all words are available again
-        unusedWords = [...difficultyWords];
-    }
+  if (unused.length === 0) {
+    console.log("All questions for this type and difficulty have been used!");
+    return null;
+  }
 
   const selected = unused[Math.floor(Math.random() * unused.length)];
   this.usedWords[difficulty][questionType].push(selected.question);
