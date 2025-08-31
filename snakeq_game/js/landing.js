60f72cb519daaf9c060317edc711e436663cbb84
Nothingkuro <josephalexii.llacer-24@cpu.edu.ph>
2025-08-31 22:00:04 +0800

    Delete some unncessary/redundant code

--- snakeq_game/js/landing.js
+++ snakeq_game/js/landing.js
@@ -190,31 +190,10 @@ class LandingPage {
       })
     }
 
-    if (copyrightBtn) {
-      copyrightBtn.addEventListener("click", () => {
-        this.playClickSound()
-        this.showCopyright()
-      })
-    }
-
-    if (closeCopyright) {
-      closeCopyright.addEventListener("click", () => {
-        this.playClickSound()
-        copyrightModal.classList.add("hidden")
-      })
-    }
-
     if (cancelBtn) {
       cancelBtn.addEventListener("click", () => {
         this.playClickSound()
         this.hideGameModeModal()
-      })
-    }
-
-    if (startBtn) {
-      startBtn.addEventListener("click", () => {
-        this.playClickSound()
-        this.startGame()
       })
     }
 
